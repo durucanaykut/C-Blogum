d8a019991c00a8643a975a8349a708c9d2d59c35
durucanaykut <durucanaykut@gmail.com>
2017-07-28 16:23:48 +0300

    İndex sayfası görüntüleme
    İndex sayfasında son eklenen 5 makelenin görüntülenmesi için gereken kodlar eklenmiştir.

--- MvcProjesi/MvcProjesi/Controllers/HomeController.cs
+++ MvcProjesi/MvcProjesi/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcProjesi.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,26 @@ namespace MvcProjesi.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        //
+        // GET: /Home/
         public ActionResult Index()
         {
             return View();
+        }
+
+        //Son 5 makalenin ana sayfaya yükleneceği Action
+        public ActionResult SonBesMakale()
+        {
+            //Veritabanından yeni bir nesne oluşturuyoruz.
+            MvcProjesiContext db = new MvcProjesiContext();
+
+            //Veritabanından sorgulamayı Linq ile yapıyoruz.
+            //Tarih sırasına göre son makaleleri OrderByDescending ile çekip Take ile de 5 tane almasını istiyoruz.
+            List<Makale> makaleListe = db.Makales.OrderByDescending(i => i.Tarih).Take(5).ToList();
+
+            //Normal içeriklerde View döndürürken, burada ise PartialView döndürüyoruz.
+            //Ayrıca makaleListe nesnesini de View'de kullanacağımız şekilde model olarak aktarıyoruz.
+            return PartialView(makaleListe);
         }
     }
 }
