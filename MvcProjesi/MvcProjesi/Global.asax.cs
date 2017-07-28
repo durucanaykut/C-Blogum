6f6879e0998bceaa588ff6f011167b33df5b1a83
durucanaykut <durucanaykut@gmail.com>
2017-07-28 16:05:21 +0300

    Blog projesi
    Projenin veri tabanı tabloları ve index sayfası içeriği oluşturuldu.

---
+++ MvcProjesi/MvcProjesi/Global.asax.cs
@@ -0,0 +1,31 @@
+﻿//Veritabanı context sınıfımızı referans veriyoruz
+using MvcProjesi.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace MvcProjesi
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        //Uygulama ilk başlatıldığında, buradaki metod çalışacak.
+        protected void Application_Start()
+        {
+            //Burada veritabanı sınıfımızdan, bir nesne oluşturuyoruz. using kullanmamızın sebebi,
+            //db nesnesinin işi bittiğinde, silinmesini ve hafızada yer tutmamasını sağlamak.
+            using (MvcProjesiContext db = new MvcProjesiContext())
+            {
+                //Bu metod, eğer veritabanımız oluşturulmamış ise, oluşturulmasını sağlıyor.
+                db.Database.CreateIfNotExists();
+            }
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
